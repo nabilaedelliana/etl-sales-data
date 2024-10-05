d44e05142565fa6937733d951e1f28a78f571839
Nabila Edelliana K <edellianakh@gmail.com>
2024-10-06 00:52:30 +0700

    Initial commit

--- dockerfile
+++ dockerfile
@@ -1,3 +1,27 @@
+#FROM python:3.9
+
+#RUN pip install --upgrade pip
+
+#RUN apt-get update && apt-get install -y tzdata
+#ENV TZ=Asia/Jakarta 
+
+#WORKDIR /app
+
+# Copy requirements and install them
+#COPY requirements.txt .
+#RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the entire solution
+#COPY . .
+
+# Set environment variables
+#ENV DATA_DIR=/data
+
+# Command to run the script
+#CMD ["python", "etlapp/scripts/ETL.py"]
+
+
+
 FROM python:3.9
 
 RUN pip install --upgrade pip
@@ -17,7 +41,7 @@ COPY . .
 # Set environment variables
 ENV DATA_DIR=/data
 
-# Command to run the script
-CMD ["python", "etlapp/scripts/ETL.py"]
-CMD ["python", "scripts/scheduler.py"]
+# Command to run the scheduler
+CMD ["python", "etlapp/scripts/scheduler.py"]
+
 
--- etlapp/scripts/scheduler.py
+++ etlapp/scripts/scheduler.py
@@ -1,9 +1,32 @@
+#import logging
+#import os
+#import schedule
+#import time
+
+# Pastikan direktori logs sudah ada
+#if not os.path.exists('logs'):
+#    os.makedirs('logs')
+
+#logging.basicConfig(filename='logs/scheduler.log', level=logging.INFO)
+
+#def job():
+#    logging.info("Starting ETL job...")
+#    os.system('python scripts/ETL.py')
+
+# Scheduler untuk menjalankan ETL setiap hari
+#schedule.every().day.at("22:43").do(job)
+
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
+
+
 import logging
 import os
 import schedule
 import time
 
-# Pastikan direktori logs sudah ada
+# Make sure the logs directory exists
 if not os.path.exists('logs'):
     os.makedirs('logs')
 
@@ -11,9 +34,16 @@ logging.basicConfig(filename='logs/scheduler.log', level=logging.INFO)
 
 def job():
     logging.info("Starting ETL job...")
-    os.system('python scripts/ETL.py')
+    try:
+        # Define the path to the ETL script
+        etl_script_path = os.path.join('etlapp', 'scripts', 'ETL.py')
+        # Run the ETL script
+        os.system(f'python {etl_script_path}')
+        logging.info("ETL job completed successfully.")
+    except Exception as e:
+        logging.error(f"ETL job failed: {e}")
 
-# Scheduler untuk menjalankan ETL setiap hari
+# Schedule the ETL to run every day at a specified time
 schedule.every().day.at("22:43").do(job)
 
 while True:
